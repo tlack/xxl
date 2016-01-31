69a963031fe74e215ca8cd10b014ecdff446dc5f
Thomas Lackner <lackner@gmail.com>
2016-01-31 06:33:56 -0500

    repr: another attempt at shortening worthless output for complex types (no more ptr addr)

--- repr.js
+++ repr.js
@@ -4,12 +4,12 @@ lib.each(lib.types, function(t) {
 	if(t[6] == true) return;
 	var tmpls = [
 		"char* repr_{{1}}(VP x,char* s,size_t sz) { int i; \n"+
-			"IF_RET(x->n==0,APF(sz,\"x{{1}}0()\",0));\n" + 
-			"if(!SIMPLE(x)) APF(sz,\"`{{1}}(\",0);\n"+
-			"else if(x->n>1) APF(sz,\"(\",0);\n"+
-			"for(i=0;i<x->n-1;i++) snprintf(s+strlen(s),sz-strlen(s)-1,\"{{5}},\",AS_{{1}}(x,i)); \n"+
-			"if(SIMPLE(x)) snprintf(s+strlen(s),sz-strlen(s)-1,\"{{5}}{{1}}\",AS_{{1}}(x,i)); \n"+
-			"else snprintf(s+strlen(s),sz-strlen(s)-1,\"{{5}}\",AS_{{1}}(x,i)); \n"+
+			"\tIF_RET(x->n==0,APF(sz,\"x{{1}}0()\",0));\n" + 
+			"\tif(!SIMPLE(x)) APF(sz,\"`{{1}}(\",0);\n"+
+			"\telse if(x->n>1) APF(sz,\"(\",0);\n"+
+			"\tfor(i=0;i<x->n-1;i++) snprintf(s+strlen(s),sz-strlen(s)-1,\"{{5}},\",AS_{{1}}(x,i)); \n"+
+			"\tif(SIMPLE(x)) snprintf(s+strlen(s),sz-strlen(s)-1,\"{{5}}{{1}}\",AS_{{1}}(x,i)); \n"+
+			"\t\telse snprintf(s+strlen(s),sz-strlen(s)-1,\"...\",AS_{{1}}(x,i)); \n"+
 			"if(!SIMPLE(x) || x->n>1) APF(sz,\")\",0);\n"+
 			"return s; }\n"
 	];
