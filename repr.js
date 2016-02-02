351df0e0f9631ca004489bcd122ee884cb9bf972
tlack <lackner@gmail.com>
2016-02-01 20:12:34 -0500

    repr type indicators for non-simple types used backtick instead of apostrophe

--- repr.js
+++ repr.js
@@ -5,7 +5,7 @@ lib.each(lib.types, function(t) {
 	var tmpls = [
 		"char* repr_{{1}}(VP x,char* s,size_t sz) { int i; \n"+
 			"\tIF_RET(x->n==0,APF(sz,\"x{{1}}0()\",0));\n" + 
-			"\tif(!SIMPLE(x)) APF(sz,\"`{{1}}(\",0);\n"+
+			"\tif(!SIMPLE(x)) APF(sz,\"'{{1}}(\",0);\n"+
 			"\telse if(x->n>1) APF(sz,\"(\",0);\n"+
 			"\tfor(i=0;i<x->n-1;i++) snprintf(s+strlen(s),sz-strlen(s)-1,\"{{5}},\",AS_{{1}}(x,i)); \n"+
 			"\tif(SIMPLE(x)) snprintf(s+strlen(s),sz-strlen(s)-1,\"{{5}}{{1}}\",AS_{{1}}(x,i)); \n"+
