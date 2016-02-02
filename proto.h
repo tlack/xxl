ac88531e4ed29c7b9e7d3163e8e805b4ca3616c2
tlack <lackner@gmail.com>
2016-02-01 20:46:01 -0500

    appendbuf() proto

--- proto.h
+++ proto.h
@@ -8,6 +8,7 @@ VP any(VP x);
 VP apply(VP x,VP y);
 VP applyctx(VP x,VP y);
 VP append(VP x,VP y);
+VP appendbuf(VP x,buf_t buf,size_t nelem);
 static VP assign(VP x,VP k,VP val);
 VP cast(VP x,VP y);
 VP capacity(VP x);
