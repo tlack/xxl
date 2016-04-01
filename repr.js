0b0fc8688b36e4ebc48bcb6b5ab3f7538ceb0659
tlack <lackner@gmail.com>
2016-04-01 03:16:04 +0000

    fix for bugs in repr_ due to MAX_ITEMS

--- repr.js
+++ repr.js
@@ -3,12 +3,12 @@ console.log(lib.prelude);
 lib.each(lib.types, function(t) {
 	if(t[6] == true) return;
 	var tmpls = [
-		"char* repr_{{1}}(VP x,char* s,size_t sz) { int i; \n"+
-			"\tIF_RET(x->n==0,FMT_into_s(sz,\"x{{1}}0()\",0));\n" + 
+		"char* repr_{{1}}(VP x,char* s,size_t sz) { int i, xn=x->n; \n"+
+			"\tIF_RET(xn==0,FMT_into_s(sz,\"x{{1}}0()\",0));\n" + 
 			"\tif(!SIMPLE(x)) { FMT_into_s(sz,\"'{{1}}(...)\",0); return s; }\n"+
-			"\telse if(x->n>1) FMT_into_s(sz,\"(\",0);\n"+
-			"\tfor(i=0;i<x->n-1;i++) {\n"+
-			"\t\tif(REPR_MAX_ITEMS && i==(REPR_MAX_ITEMS/2)) {\n"+
+			"\telse if(xn>1) FMT_into_s(sz,\"(\",0);\n"+
+			"\tfor(i=0;i<xn-1;i++) {\n"+
+			"\t\tif(REPR_MAX_ITEMS && xn>(REPR_MAX_ITEMS*2) && i==(xn-REPR_MAX_ITEMS)/2) {\n"+
 			"\t\t\tFMT_into_s(sz,\".. (%d omitted) ..\", (x->n)-REPR_MAX_ITEMS);\n"+
 			"\t\t\ti+=REPR_MAX_ITEMS;\n"+
 			"\t\t\tcontinue;}\n"+
